bab2d1584cdaf4297e65f3fee6b580157d4ea222
Denise <57792228+denisebartling@users.noreply.github.com>
2021-11-04 14:56:48 +0100

    Add files via upload

---
+++ Frontend 21/scripts/script.js
@@ -0,0 +1,29 @@
+var i = 0; 			/* Start Point */
+var images = [];	/* Images Array */
+var time = 6000;	/* Time Between Switch */
+	 
+/* Image List */
+images[0] = "images/banner1.jpg";
+images[1] = "images/banner2.jpg";
+
+/* Change Image */
+function changeImg(){
+	document.slide.src = images[i];
+
+	/* Check If Index Is Under Max */
+	if(i < images.length - 1){
+	  /* Add 1 to Index */
+	  i++; 
+	} else { 
+		/* Reset to O */
+		i = 0;
+	}
+
+	/* Run function every x seconds */
+	setTimeout("changeImg()", time);
+}
+
+/* Run function when page loads */
+window.onload=changeImg;
+
+/* Bron: https://www.youtube.com/watch?v=4YQ4svkETS0&ab_channel=TraversyMedia */
